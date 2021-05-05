3d185372dcd482f0d37da2c841b8ce746a010535
Manolo Santos <manolo.santos@gmail.com>
2021-05-05 23:14:02 +0200

    Favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,6 +5,7 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
           <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
         </Head>
         <body>
