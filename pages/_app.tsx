91115339a09445820ce7496ee49c9d6ca97cb9c8
Manolo Santos <manolo.santos@gmail.com>
2021-05-21 16:59:26 +0200

    Convert App to typeScript

---
+++ pages/_app.tsx
@@ -0,0 +1,9 @@
+import "../styles/globals.css";
+import React from "react";
+import { AppProps } from "next/app";
+
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  return <Component {...pageProps} />;
+};
+
+export default MyApp;
